1f959be000639d41f703ec30ad8faa84d7a5f9ab
YHS <sva24fur@gmail.com>
2025-01-21 18:17:05 +0900

    airflow

---
+++ airflow/dags/utils/config.py
@@ -0,0 +1,9 @@
+TOP_DIR = '/home/harry/airflow/dags/outputs'
+CURRENT_DIR = '/home/harry/airflow/dags/outputs'
+
+DB_NAME = 'test.db'
+
+BATCH_SIZE = 32
+NUM_EPOCHS = 10
+LEARNING_RATE = 0.001
+USE_ATTENTION = True
