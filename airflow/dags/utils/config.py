12350ba87f68b9af0b79265b51fb141f10270017
kanevvv <sva24fur@gmail.com>
2025-01-24 11:04:13 +0900

    Delete airflow directory

--- airflow/dags/utils/config.py
+++
@@ -1,9 +0,0 @@
-TOP_DIR = '/home/harry/airflow/dags/outputs'
-CURRENT_DIR = '/home/harry/airflow/dags/outputs'
-
-DB_NAME = 'test.db'
-
-BATCH_SIZE = 32
-NUM_EPOCHS = 10
-LEARNING_RATE = 0.001
-USE_ATTENTION = True
